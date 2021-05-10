aaa101d26fa19721e976300a864510d331078ae6
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-05-10 16:37:05 +0200

    js added forecast and geolocation

--- js/script.js
+++ js/script.js
@@ -1,19 +1,56 @@
+window.addEventListener("load", () => {
+    let long;
+    let lat;
+
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(position => {
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+        });
+
+    }
+
 const apiKey = "939409706965cabeb39bb19b3d90a4cc";
-const searchInput = document.querySelector("#search-field");
-const searchBtn = document.querySelector("#search-btn");
+const searchInput = document.getElementById("search-field");
+const searchBtn = document.getElementById("search-btn");
+const mainTemp = document.getElementById("main-temp");
+const feelsLike = document.getElementById("feels-like");
+const descriptionNow = document.getElementById("description-now")
+const minTemp = document.getElementById("temp-min");
+const maxTemp = document.getElementById("temp-max");
 
 searchBtn.addEventListener("click", () => {
-    let citySearch = searchInput.value;
-    console.log(citySearch);
+    let cityName = searchInput.value;
+    console.log(cityName);
 
-const getWeather = async () => {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}`);
-    const data = await response.json();
-    console.log(data);
+    const getWeather = async () => {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
+        const data = await response.json();
+        console.log(data);
+        console.log(data.main.temp);
+        console.log(data.main.temp_max);
+        console.log(data.main.temp_min);
+
+        mainTemp.innerHTML = `Current temperature ${data.main.temp}°`;
+        feelsLike.innerHTML = `Feels like ${data.main.feels_like}°`;
+        descriptionNow.innerHTML = `${data.weather[0].description}`;
+
+        minTemp.innerHTML = `Min temp ${data.main.temp_min}°`;
+        maxTemp.innerHTML = `Max temp ${data.main.temp_max}°`;
     }
     getWeather();
-})
 
+
+    const forecast = async () => {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`);
+        const forecast = await response.json();
+        console.log(forecast);
+    }
+
+    forecast();
+});
+
+});
 
 
 
