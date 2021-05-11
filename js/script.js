173b0a1d01f0b087ddff84e1a3a75c8b2b72faeb
Mick Dellaert <82022124+MickDellaert@users.noreply.github.com>
2021-05-11 15:48:43 +0200

    rewritten code and decluttered a bit with the use of a callback function so not everything is inside one event call, added css, add comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//Function to get current geolocation, currently not used
+
 /*window.addEventListener("load", () => {
     let long;
     let lat;
@@ -13,55 +15,62 @@
 const apiKey = "939409706965cabeb39bb19b3d90a4cc";
 const searchInput = document.getElementById("search-field");
 const searchBtn = document.getElementById("search-btn");
+const locationfield = document.getElementById("location-current");
 const mainTemp = document.getElementById("main-temp");
 const feelsLike = document.getElementById("feels-like");
 const descriptionNow = document.getElementById("description-now")
 const minTemp = document.getElementById("temp-min");
 const maxTemp = document.getElementById("temp-max");
 
+//Main function to get data from the openweather api
+const getWeather = async (cityName, callback) => {
+    const data1 = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
+    const currentWeather = await data1.json();
+
+    let lon = currentWeather.coord.lon;
+    let lat = currentWeather.coord.lat;
+
+    const data2 = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=metric`);
+    const forecast = await data2.json();
+
+    console.log(currentWeather);
+    console.log(forecast);
+
+    callback(currentWeather, forecast);
+}
+
+//Button that gets the user input and start the function to add information to HTML
 searchBtn.addEventListener("click", () => {
     let cityName = searchInput.value;
     console.log(cityName);
 
-    const getWeather = async () => {
-        const data1 = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`);
-        const currentWeather = await data1.json();
+//Function that gets the weather information via a callback function and puts it in the HTML DOM
+        getWeather(cityName,(currentWeather, forecast) => {
+            locationfield.innerHTML = currentWeather.name;
+            mainTemp.innerHTML = `<p>Current temperature ${forecast.daily[0].temp.day}°</p>`;
+            feelsLike.innerHTML = `Feels like ${forecast.current.feels_like}°`;
+            descriptionNow.innerHTML = `${forecast.current.weather[0].description}`;
+            minTemp.innerHTML = `Min temp ${forecast.daily[0].temp.min}°`;
+            maxTemp.innerHTML = `Max temp ${forecast.daily[0].temp.max}°`;
 
-        let lon = currentWeather.coord.lon;
-        let lat = currentWeather.coord.lat;
+            for (let i = 1; i < 7; i++) {
+                console.log(forecast.daily[i].weather[0].description);
+                console.log(forecast.daily[i].temp.day)
+                console.log(forecast.daily[i].temp.max)
+                console.log(forecast.daily[i].temp.min)
+            }
 
-        const data2 = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=metric`);
-        const forecast = await data2.json();
+            document.getElementById("forecast-row").innerHTML = "";
 
-        let allData = {...currentWeather,...forecast};
-
-        console.log(currentWeather);
-        console.log(forecast);
-
-        mainTemp.innerHTML = `Current temperature ${currentWeather.main.temp}°`;
-        feelsLike.innerHTML = `Feels like ${currentWeather.main.feels_like}°`;
-        descriptionNow.innerHTML = `${currentWeather.weather[0].description}`;
-        minTemp.innerHTML = `Min temp ${currentWeather.main.temp_min}°`;
-        maxTemp.innerHTML = `Max temp ${currentWeather.main.temp_max}°`;
-
-        for (let i = 1; i < 7; i++) {
-            console.log(forecast.daily[i].weather[0].description);
-            console.log(forecast.daily[i].temp.day)
-            console.log(forecast.daily[i].temp.max)
-            console.log(forecast.daily[i].temp.min)
-        }
-
-        for (let i = 1; i < 7; i++) {
-            let content = `${allData.daily[i].weather[0].description}`;
-
-            newDiv = document.createElement("div")
-            document.getElementById("forecast-row").appendChild(newDiv);
-            newDiv.className = "col forecast";
-            newDiv.innerText = content;
-
-        }
-        }
-        getWeather();
+            for (let i = 1; i < 7; i++) {
+                let content1 = forecast.daily[i].temp.day;
+                let content2 = `<p class="description-forecast">${forecast.daily[i].weather[0].description}</p>`;
+                newDiv = document.createElement("div")
+                document.getElementById("forecast-row").appendChild(newDiv);
+                newDiv.className = "col forecast";
+                newDiv.innerHTML = content1 + content2;
+            }
+        });
     });
 
 
